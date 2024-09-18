9df24d834b2f57b64715526957b18dd3f5d0c807
Poovarasan <poovarasan936161@gmail.com>
2024-09-18 19:29:39 +0530

    hosting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import  {useState}  from  'react';
 function App() {
+  const[result,setResult]=useState(0);
+  const[input,setInput]=useState(0);
+  const[history,setHistory]=useState('0');
+ // useEffect(()=>
+ // {
+   // setResult(p=>p);
+ // });
+
+ const plus=()=>{
+  setResult(p=>Number(p)+Number(input));
+  setHistory(a=>a+"+"+input);
+  // setInput('');
+ }
+
+ const minus=()=>{
+  setResult(p=>Number(p)-Number(input));
+  setHistory(a=>a+"-"+input);
+  // setInput('');
+ }
+ const multi=()=>{
+  setResult(p=>Number(p)*Number(input));
+  setHistory(a=>a+"*"+input);
+  // setInput('');
+ }
+ const div=()=>{
+  setResult(p=>Number(p)/Number(input));
+  setHistory(a=>a+"/"+input);
+  //setInput('');
+ }
+ const inputReset=()=>{
+  setInput('');
+
+ }
+ const resultReset=()=>{
+  setResult(0);
+  setHistory('0');
+
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>{result}</h1><p>{history}</p>
+    <input type='number'  onChange={e=>setInput(e.target.value)} value={input}></input> 
+ <br></br>
+ <br></br>
+ 
+ <button onClick={plus}>Add</button>
+ <button onClick={minus}>Sub</button>
+ <button onClick={multi}>Mul</button>
+ <button onClick={div}>Div</button>
+ <br></br>
+ <br></br>
+ <button onClick={inputReset}>Reset Input</button>
+ <button onClick={resultReset}>Reset Result</button>
+         </div>
   );
 }
 
